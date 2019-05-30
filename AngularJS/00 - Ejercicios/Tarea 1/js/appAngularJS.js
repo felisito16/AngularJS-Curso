b500b5581c569d3a6e62b31a098eed141a555b1f
Felix <felix.delrio.1@gmail.com>
2019-05-30 16:59:09 +0200

    VENGA AHI

--- AngularJS/00 - Ejercicios/Tarea 1/js/appAngularJS.js
+++ AngularJS/00 - Ejercicios/Tarea 1/js/appAngularJS.js
@@ -1,23 +1,24 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope, $filter) {
+var app = angular.module('myApp', ['ngStorage']);
+app.controller('controller', function ($scope, $localStorage, $filter) {
+
+    /* Comprobamos si existen datos en el localStorage, exactamente 
+    el item listaTareas, si existe asignamos los valores de este
+    al scope local y si no, lo declaramos. */
+    $scope.listaTareasLocal = [];
+    if ($localStorage.listaTareas) {
+        // Any what else?
+        $scope.listaTareasLocal = $localStorage.listaTareas;
+    } else {
+        $localStorage.listaTareas = [];
+    }
 
     /* Variable que guarda la lista de tareas que 
     visualizaremos en la tabla */
-    $scope.listaTareas = [{
-        "autor": "John",
-        "desc": "Limpieza del cuarto de baño",
-        "fecha": "28/05/2019",
-        "check": false
-    },
-    {
-        "autor": "Lucía",
-        "desc": "Sacar a los perros a pasear",
-        "fecha": "26/05/2019",
-        "check": true
-    }];
+    /* $localStorage.listaTareas = []; */
 
     /* Por defecto, la vista de edicion
     inhabilitada y boton de Añadir habilitado*/
+    var dosNo = false
     $scope.noEditable = true
     $scope.apagarBoton = false
 
@@ -30,8 +31,10 @@ app.controller('myCtrl', function ($scope, $filter) {
 
     $scope.Delete = function (index) {
         /* Hacemos un slice con el index
-        actual seleccionado de la tarea */
-        $scope.listaTareas.splice(index, 1)
+        actual seleccionado de la tarea tanto
+        en el scope como en el localStorage */
+        $localStorage.listaTareas.splice(index, 1)
+        $scope.listaTareasLocal.splice(index, 1)
     }
 
     $scope.Cancelar = function () {
@@ -61,16 +64,16 @@ app.controller('myCtrl', function ($scope, $filter) {
         var i = $scope.indiceEditar
         // Pasamos lo valores actuales a la vista de 
         // edicion
-        $scope.valueAutor = $scope.listaTareas[i].autor
-        $scope.valueDesc = $scope.listaTareas[i].desc
-        //$scope.valueFecha = $scope.listaTareas[i].fecha
-        $("label:eq(2)").text("Fecha original: " + $scope.listaTareas[i].fecha)
-        $scope.valueCheck = $scope.listaTareas[i].check
+        $scope.valueAutor = $scope.listaTareasLocal[i].autor
+        $scope.valueDesc = $scope.listaTareasLocal[i].desc
+        //$scope.valueFecha = $scope.listaTareasLocal[i].fecha
+        $("label:eq(2)").text("Fecha original: " + $scope.listaTareasLocal[i].fecha)
+        $scope.valueCheck = $scope.listaTareasLocal[i].check
 
         $("td a").hide()
     }
-    $scope.Save = function () {
-
+    $scope.save = function () {
+        console.log($scope.valueCheck);
         // Dependiendo de si entramos en la vista de edicion
         // en modo Editar o Crear nuvea tarea
         // El guardar funcionara de una manera u otra
@@ -81,14 +84,32 @@ app.controller('myCtrl', function ($scope, $filter) {
             /* Formateamos la fecha del input de edicion y la guardamos
             en otra variable a parte */
             $scope.valueFechaGG = $filter('date')($scope.valueFecha, "dd/MM/yyyy");
-            /* Hacemos push en la variable de la lista de las tareas */
-            $scope.listaTareas.push({
+            !$scope.valueCheck ? $scope.valueCheck = false : $scope.valueCheck = true
+
+            /* Hacemos push en la variable de la lista de las tareas 
+            en el localStorage */
+            $localStorage.listaTareas.push({
                 "autor": $scope.valueAutor,
                 "desc": $scope.valueDesc,
                 "fecha": $scope.valueFechaGG,
                 "check": $scope.valueCheck
             })
+            /* Cogemos el length de la lista en el localStorage para 
+            saber el id de la nueva tarea a añadir en el scope que 
+            va a suministrar a la tabla de las tareas */
+            var localListaLength = ($localStorage.listaTareas.length - 1)
+
+            /* Y aqui le hacemos push al scope intermediario */
+            $scope.listaTareasLocal.push({
+                "autor": $localStorage.listaTareas[localListaLength].autor,
+                "desc": $localStorage.listaTareas[localListaLength].desc,
+                "fecha": $localStorage.listaTareas[localListaLength].fecha,
+                "check": $localStorage.listaTareas[localListaLength].check
+            })
+
             $("label:eq(2)").text("Fecha de la tarea")
+
+
 
         } else {
             // Editando una tarea
@@ -96,22 +117,33 @@ app.controller('myCtrl', function ($scope, $filter) {
             /* guardamos en "i" el indice de la tarea que estamos editando
              y volvemos a formatear la fecha nueva*/
             var i = $scope.indiceEditar
+            console.log(i)
             $scope.valueFechaGG = $filter('date')($scope.valueFecha, "dd/MM/yyyy");
 
-            /* Asignamos los nuevos valores a la tarea que estamos editando */
-            $scope.listaTareas[i].autor = $scope.valueAutor
-            $scope.listaTareas[i].desc = $scope.valueDesc
+            /* Asignamos los nuevos valores a la tarea de la lista de tareas del 
+            localStorage que estamos editando  */
+            $localStorage.listaTareas[i].autor = $scope.valueAutor
+            $localStorage.listaTareas[i].desc = $scope.valueDesc
             if ($scope.valueFecha == undefined
                 || $scope.valueFechaGG == undefined
                 || $scope.valueFechaGG == ""
                 || $scope.valueFechaGG == null) {
                 // Any What else?
             } else {
-                $scope.listaTareas[i].fecha = $scope.valueFechaGG
+                $localStorage.listaTareas[i].fecha = $scope.valueFechaGG
             }
-            $scope.listaTareas[i].check = $scope.valueCheck
+            $localStorage.listaTareas[i].check = $scope.valueCheck
 
+            /* Redefinimos el valor de valueFechaGG para la proxima vez que
+            editemos */
             $scope.valueFechaGG = undefined
+
+            /* Por ultimo guardamos en el scope los datos del localStorage */
+            $scope.listaTareasLocal[i].autor = $localStorage.listaTareas[i].autor
+            $scope.listaTareasLocal[i].desc = $localStorage.listaTareas[i].desc
+            $scope.listaTareasLocal[i].fecha = $localStorage.listaTareas[i].fecha
+            $scope.listaTareasLocal[i].check = $localStorage.listaTareas[i].check
+
             $("label:eq(2)").text("Fecha de la tarea")
         }
 
@@ -126,17 +158,18 @@ app.controller('myCtrl', function ($scope, $filter) {
         $("td a").show()
         $("label:eq(2)").text("Fecha de la tarea")
         $scope.apagarBoton = false
-
     }
 
     $scope.setColorRow = function (index, classYes, classNo) {
         /* Si la tarea que esta mostrando la tabla, su campo check
         esta en true (Realizada) se mostrara en verde su fondo, sino
         en color rojo */
-        if ($scope.listaTareas[index].check == true) {
+        if ($scope.listaTareasLocal[index].check == true) {
             return classYes
-        } else {
+        } else if ($scope.listaTareasLocal[index].check == false) {
             return classNo
+        } else {
+            // Any what else?
         }
     }
 
