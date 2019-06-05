4a7761a91d4145f572650daedda75baa95d810f8
Felix <felix.delrio.1@gmail.com>
2019-06-05 16:59:15 +0200

    Muchas cosas PEPE

---
+++ AngularJS/00 - Ejercicios/Test 2/js/grid.js
@@ -0,0 +1,22 @@
+var app = angular.module('angular', []);
+app.controller('controller', ['$scope', '$timeout', function ($scope, $timeout) {
+
+    /* varible de estado del gif de loading */
+    $scope.estadoLoad = false
+
+    /* Funciona de carga del gif */
+    $scope.onLoad = function () {
+        $scope.estadoLoad = true
+        $("#buttonLogin").css({ "padding": 0 })
+
+        /* Volver a dejar como estaba */
+        $timeout(function () {
+            $("#buttonLogin").css({ "padding": "" });
+            $(".btn-success:focus").blur()
+            $scope.estadoLoad = false
+        }, 2500);
+
+    }
+
+}])
+
